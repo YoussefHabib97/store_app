3b7475d4a1ec2573fb22f27c22a8d2874823cb99
Youssef <ykmhabib97@gmail.com>
2024-03-31 19:51:58 +0200

    Created & finished product model

---
+++ lib/models/product.dart
@@ -0,0 +1,48 @@
+class Product {
+  final String id;
+  final String title;
+  final String price;
+  final String description;
+  final String image;
+  final String category;
+  final Rating? rating;
+
+  const Product({
+    required this.id,
+    required this.title,
+    required this.price,
+    required this.description,
+    required this.image,
+    required this.category,
+    required this.rating,
+  });
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      id: json['id'],
+      title: json['title'],
+      price: json['price'],
+      description: json['description'],
+      image: json['image'],
+      category: json['category'],
+      rating: json['rating'] == null ? null : Rating.fromJson(json['rating']),
+    );
+  }
+}
+
+class Rating {
+  final num rate;
+  final int count;
+
+  const Rating({
+    required this.rate,
+    required this.count,
+  });
+
+  factory Rating.fromJson(Map<String, dynamic> json) {
+    return Rating(
+      rate: json['rate'],
+      count: json['count'],
+    );
+  }
+}
